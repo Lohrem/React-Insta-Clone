7a019850716a5ddefc4c1e6e4057775268082df7
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-05-22 22:49:09 -0400

    [initial commit] created the react app

---
+++ instagram-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import SearchBar from '../src/components/SearchBar/SearchBar'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SearchBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SearchBar.css'
+import logo from '../../imgs/logo.png'
+
+const SearchBar = () => {
+  return (
+    <header className="header">
+      <img src={logo} className="logo" alt="Instagram Logo"></img>
+    </header>
+  )
+
+}
+
+export default SearchBar
