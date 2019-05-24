f4bef829e2717f60b1c15357d6360e96e35e42a9
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-05-23 22:13:21 -0400

    {PR Commit}

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import SearchBar from '../src/components/SearchBar/SearchBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <SearchBar />
-      </header>
     </div>
   );
 }
--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './SearchBar.css'
 import logo from '../../imgs/logo.png'
+import insta from '../../imgs/insta.PNG'
 
 const SearchBar = () => {
   return (
     <header className="header">
       <img src={logo} className="logo" alt="Instagram Logo"></img>
+      <div className="break"></div>
+      <img src={insta} className="insta" alt="Instagram"></img>
+      <input type="text" placeholder="Search..." className="search-input"></input>
     </header>
   )
 
