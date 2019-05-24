60fbc91092eef59fb98ea70b01cff1eae0e1c71a
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-05-24 01:45:38 -0400

    completed header design

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -10,6 +10,9 @@ const SearchBar = () => {
       <div className="break"></div>
       <img src={insta} className="insta" alt="Instagram"></img>
       <input type="text" placeholder="Search..." className="search-input"></input>
+      <a href="index.html"><i class="far fa-compass"></i></a>
+      <a href="index.html"><i class="far fa-heart"></i></a>
+      <a href="index.html"><i class="far fa-user"></i></a>
     </header>
   )
 
